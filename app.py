e647ea15c0b577730571d6fd61da4ca17bb6dfa0
RockAndSolt <156660771+RockAndSolt@users.noreply.github.com>
2024-01-15 19:09:02 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, about
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+	ApiClient, Configuration, MessagingApi,
+	ReplyMessageRequest, PushMessageRequest,
+	TextMessage, PostbackAction
+)
+from linebot.v3.webhooks import (
+	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
+)
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+## 環境変数を変数に割り当て
+CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
